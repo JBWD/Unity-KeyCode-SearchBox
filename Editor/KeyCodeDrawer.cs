066349a9019bf8f07c83f7d79e583dc1ede32dce
Weston Glasser <35278058+JBWD@users.noreply.github.com>
2024-01-29 22:37:06 -0600

    KeyCode Drawer Initial Code Commit

---
+++ Editor/KeyCodeDrawer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using Unity.VisualScripting.YamlDotNet.Core.Tokens;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+namespace Halcyon
+{
+    [CustomPropertyDrawer(typeof(KeyCode))]
+    public class KeyCodeDrawer :PropertyDrawer
+    {
+
+        private int m_value;
+        private bool initialized = false;
+        
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (!initialized)
+            {
+                m_value = property.enumValueIndex;
+                initialized = true;
+            }
+            else
+            {
+                property.enumValueIndex = m_value;
+            }
+            
+            
+            var names = Enum.GetNames(typeof(KeyCode));
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField(property.displayName, GUILayout.ExpandWidth(false));
+            if (GUILayout.Button($"{names[property.enumValueIndex]}", EditorStyles.popup))
+            {
+                var so = ScriptableObject.CreateInstance<StringListSearchProvider>();
+                so.Initialize("KeyCodes", names.ToList(),
+                    (x) => { m_value = x;});
+                
+                SearchWindow.Open(new SearchWindowContext(GUIUtility.GUIToScreenPoint(Event.current.mousePosition)),so);
+            }
+            EditorGUILayout.EndHorizontal();
+            
+        }
+    }
+}
---
+++ Editor/SearchWindow/StringListSearchProvider.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+namespace Halcyon
+{
+    public class StringListSearchProvider :ScriptableObject, ISearchWindowProvider
+    {
+        private string title;
+        private List<string> items;
+
+        private Action<int> onSetIndexCallback;
+        public void Initialize(string title, List<string> items, Action<int> onSetIndexCallback)
+        {
+            this.title = title;
+            this.items = items;
+            this.onSetIndexCallback = onSetIndexCallback;
+        }
+        
+        public List<SearchTreeEntry> CreateSearchTree(SearchWindowContext context)
+        {
+            List<SearchTreeEntry> searchList = new List<SearchTreeEntry>();
+            searchList.Add(new SearchTreeGroupEntry(new GUIContent(title),0));
+            
+            for (var index = 0; index < items.Count; index++)
+            {
+                
+                var item = items[index];
+                SearchTreeEntry entry = new SearchTreeEntry(new GUIContent(item));
+                entry.level = 1;
+                entry.userData = index;
+                searchList.Add(entry);
+            }
+            
+            return searchList;
+        }
+
+        public bool OnSelectEntry(SearchTreeEntry SearchTreeEntry, SearchWindowContext context)
+        {
+            onSetIndexCallback?.Invoke((int)SearchTreeEntry.userData);
+            return true;
+        }
+    }
+}
